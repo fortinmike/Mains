71ffbf959a0f4f370cf6230ee0461d6727250bcd
Michaël Fortin <michael@irradiated.net>
2026-06-15 15:36:12 -0400

    Initial Commit

---
+++ Power Notifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Power Notifier
+//
+//  Created by Michaël Fortin on 2026-06-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
